31d2a9dd45e85ead234e4b208115856556dbfadc
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-08 16:38:10 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,12 @@
-clear
+#include <unistd.h>
+
+/**
+ * main - prints in C
+ *
+ *Return: Always returns 1 by default
+ */
+int main(void)
+{
+     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+     return (1);
+}
