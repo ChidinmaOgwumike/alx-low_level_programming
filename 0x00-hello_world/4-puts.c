072f54abf2233fbb202df38136f023e6a35c135c
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-08 15:59:17 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Printing in C Language
+*
+*Return: Always returns 0 by default
+*/
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
