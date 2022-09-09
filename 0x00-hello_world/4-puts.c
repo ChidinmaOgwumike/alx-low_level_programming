49403adcd1e5383a4379d2a8acbdb2f4124b6901
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-08 17:44:35 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - print in C
+* main - print the string in the put function
 *
+*Description: main function in use
+*this program will print "Programming is like building a multilingual puzzle
 *Return: 0
 */
 int main(void)
 {
+        puts("
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
