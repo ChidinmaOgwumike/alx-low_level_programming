a746f26ddd39e77c019c77343bac3ad964b413fb
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-09 08:14:43 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 
+#include <stdio.h>
 /**
-* main - print the string in the put function
+* main print the string in the put function
 *
-*Description: main function in use
-*this program will print "Programming is like building a multilingual puzzle
-*Return: 0
+* Description: using the main function
+* this program prints ""Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
-        puts("
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of many var types
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+      char a;
+      int b;
+      long init c;
+      long long int d;
+      float e;
+
