11ee045c46c4990f74fd0d69d3eaa8a33649f6e2
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-11 15:06:03 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <unistd.h>
-
 /**
-* main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
+ * main - prints text
+ *
+ * Return: 1
+ */
 int main(void)
 {
-	puts(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
