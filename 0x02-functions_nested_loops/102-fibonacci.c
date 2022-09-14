5103e531c62239a1519f7c0bbdac8504c13cdd05
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-14 16:55:23 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 /**
- * main - always
- * Return: always
+ *main - prints out first 50
+ *fibonacci suit numbers
+ *Return: return 0
  */
 int main(void)
 {
-	long int i, j, k, l;
-	j = 1;
-	k = 2;
-	for (i = 1; i <= 50; ++i)
-	{
-		if (j != 20365011074)
-		{
-			printf("%ld, ", j);
-		} else
-		{
-			printf("%ld\n", j);
-		}
-		l = j + k;
-		j = k;
-		k = l;
-	}
-	return (0);
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+for (inc = 0; inc < 50; inc++)
+{
+n3 = n1 + n2;
+printf("%lu", n3);
+n1 = n2;
+n2 = n3;
+if (inc == 49)
+printf("\n");
+else
+printf(", ");
 }
