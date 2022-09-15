d46827120c474932a14c947e359120555d6ad8db
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-15 14:53:26 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,7 @@
  * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and resets mins
+ * Return: 0
  */
 void jack_bauer(void)
 {
@@ -23,8 +24,9 @@ void jack_bauer(void)
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
+	_putchar('\n');
 	}
 	hours++;
 	minutes = 0;
-	}
+}
 }
