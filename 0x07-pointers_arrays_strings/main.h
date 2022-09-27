02506ee2a5e306a4720ece17dd2609c9d4347c89
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-27 05:47:28 -0700

    Work

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-/**
-* void prototypes()
-* int prototypes()
-*/
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
