d3705c244ea09524804725b72cccd450fabc81ff
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-28 07:37:56 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - length of a string
  * @s: pointer block of memory to fill
- * @Return: length of string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
