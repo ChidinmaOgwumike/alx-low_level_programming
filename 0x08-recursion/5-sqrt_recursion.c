38c9c5f9b0cf85a55f4846ceae73c32a03af1c38
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-28 14:25:04 -0700

    Please work 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,15 +16,15 @@ int _sqrt_recursion(int n)
 /**
  * _sqrt - returns the square root of a number
  * @n: test number
- * @c: squared number
+ * @x: squared number
  *
  * Return: the square root of n
  */
-int _sqrt(int n, int c)
+int _sqrt(int n, int x)
 {
-	if (n > c / 2)
+	if (n > x / 2)
 		return (-1);
-	else if (n * n == c)
+	else if (n * n == x)
 		return (n);
-	return (_sqrt(n + 1, c));
+	return (_sqrt(n + 1, x));
 }
