ed7b2ee8d91fa5a758f7fecf658d7108704476c4
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-09-30 15:48:32 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,10 @@
 int main(int argc, char *argv[])
 {
 int a, b;
-if (argc -- 3)
+if (argc == 3)
 {
-a - atoi(argv[1]);
-b - atoi(argv[2]);
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 printf("%d\n", a *b);
 return (0);
 }
