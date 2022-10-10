8b8fae5141c75812890c208bcdf1dcb9c3f44891
ChidinmaOgwumike <chidinmaogwumike@gmail.com>
2022-10-10 05:31:49 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -68,15 +68,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-		doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-		if (doggo->owner == NULL)
-		{
-			free(doggo->name);								free(doggo);
-			return (NULL);								}
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
 
-		doggo->name = _strcopy(doggo->name, name);
-		doggo->age = age;
-		doggo->owner = _strcopy(doggo->owner, owner);
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
 
-		return (doggo);
+	return (doggo);
 }
